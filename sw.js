06a0b155606ea2b3766131b1700446a15b65b167
WC26TIMER <spiritaviationdirect@gmail.com>
2026-06-09 18:55:19 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,51 @@
-const CACHE_NAME = 'wc26-timer-v1';
+const CACHE_NAME = 'wc26-timer-v2';
+
 const ASSETS = [
   '/26TIMER/',
   '/26TIMER/index.html',
   '/26TIMER/manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
   );
+
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
+      );
+
     })
+
   );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+
+  );
+
 });
